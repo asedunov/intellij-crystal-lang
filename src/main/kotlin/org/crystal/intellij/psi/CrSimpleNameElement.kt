41b9a0103537f6d64356fc1c9147979e56cab56a
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2021-08-01 00:30:52 +0300

    Analysis: Fix name retrieval for instance/class/global variables

--- src/main/kotlin/org/crystal/intellij/psi/CrSimpleNameElement.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrSimpleNameElement.kt
@@ -3,13 +3,22 @@ package org.crystal.intellij.psi
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.util.elementType
+import org.crystal.intellij.lexer.CR_CLASS_VAR
+import org.crystal.intellij.lexer.CR_GLOBAL_VAR
+import org.crystal.intellij.lexer.CR_INSTANCE_VAR
 
 class CrSimpleNameElement(node: ASTNode) : CrNameElement(node) {
     override fun accept(visitor: CrVisitor) = visitor.visitSimpleNameElement(this)
 
     override fun getName(): String? {
         val e = firstChild
-        return if (e is CrStringLiteralExpression) e.stringValue else text
+        if (e is CrStringLiteralExpression) return e.stringValue
+        val name = text
+        return when (e.elementType) {
+            CR_INSTANCE_VAR, CR_GLOBAL_VAR -> name.substring(1)
+            CR_CLASS_VAR -> name.substring(2)
+            else -> name
+        }
     }
 
     val tokenType: IElementType?

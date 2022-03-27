875b55a5a95e582b97499d27384928672b756ddf
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-03-27 09:05:12 +0200

    Analysis: Disable dynamic statement checks inside calls
    This can be revised when proper macro resolve is implemented
    
    (cherry picked from commit cf0c9a2f7384faedf6e5b5342a043ca69924d3da)

---
+++ src/testData/analysis/nonDynamics/calls.cr
@@ -0,0 +1,35 @@
+foo do
+  require "require"
+
+  include Include
+
+  extend Extend
+
+  Const = 1
+
+  def def
+  end
+
+  class Class
+  end
+
+  struct Struct
+  end
+
+  module Module
+  end
+
+  enum Enum
+  end
+
+  lib Lib
+  end
+
+  alias Alias = Type
+
+  annotation Annotation
+  end
+
+  macro macro
+  end
+end

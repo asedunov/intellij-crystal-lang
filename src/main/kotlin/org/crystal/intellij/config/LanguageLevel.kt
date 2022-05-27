aab3f55299778da6493a26cd4453d841036f996f
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-05-27 21:50:40 +0200

    Configuration: Introduce 1.4 language level
    (cherry picked from commit f9b4778ba02d100ae2a4bfefff2604813d4e20b6)

--- src/main/kotlin/org/crystal/intellij/config/LanguageLevel.kt
+++ src/main/kotlin/org/crystal/intellij/config/LanguageLevel.kt
@@ -5,10 +5,11 @@ enum class LanguageLevel(val shortName: String) {
     CRYSTAL_1_1("1.1"),
     CRYSTAL_1_2("1.2"),
     CRYSTAL_1_3("1.3"),
+    CRYSTAL_1_4("1.4"),
     CRYSTAL_PREVIEW("Preview");
 
     companion object {
         @JvmField
-        val LATEST_STABLE = CRYSTAL_1_3
+        val LATEST_STABLE = CRYSTAL_1_4
     }
 }

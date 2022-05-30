b84a9b900777ff30386cad8dbd3b065d4c3f74c5
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-05-30 10:29:27 +0200

    Parser: Support leading :: in proc pointers
    [LANG: 40d7a2af77c36662c4adbe70434ab76d7b6aba2b]

--- src/main/kotlin/org/crystal/intellij/psi/CrFunctionPointerExpression.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrFunctionPointerExpression.kt
@@ -1,7 +1,15 @@
 package org.crystal.intellij.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import org.crystal.intellij.lexer.CR_PATH_OP
 
 class CrFunctionPointerExpression(node: ASTNode) : CrExpressionImpl(node) {
     override fun accept(visitor: CrVisitor) = visitor.visitFunctionPointerExpression(this)
+
+    val globalOp: PsiElement?
+        get() = findChildByType(CR_PATH_OP)
+
+    val receiver: CrMethodReceiver?
+        get() = childOfType()
 }
---
+++ src/testData/analysis/globalFunctionPointers.cr
@@ -0,0 +1,9 @@
+->::foo
+
+->::Foo::Bar.foo
+
+-><error descr="ProcPointer of local variable cannot be global">::</error>foo.foo
+
+-><error descr="ProcPointer of instance variable cannot be global">::</error>@foo.foo
+
+-><error descr="ProcPointer of class variable cannot be global">::</error>@@foo.foo

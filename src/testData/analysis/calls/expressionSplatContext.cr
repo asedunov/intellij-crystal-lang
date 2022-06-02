217dd04480d770b6b45526962090a0cfa84ce40e
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-06-02 14:52:32 +0200

    Analysis: Report splats used as RHS of non-call assignments
    (cherry picked from commit 716ee318c987cb09514cdedd76e6eda6d69a8161)

--- src/testData/analysis/calls/expressionSplatContext.cr
+++ src/testData/analysis/calls/expressionSplatContext.cr
@@ -1,5 +1,14 @@
 foo(*c)
 
+a = <error descr="Splat argument is not allowed here">*</error>b
+a, a.x = <error descr="Splat argument is not allowed here">*</error>b
+a.x, a.y = *b
+a =(<error descr="Splat argument is not allowed here">*</error>b)
+a = (<error descr="Splat argument is not allowed here">*</error>b)
+a[0] = *b
+a[0], a = <error descr="Splat argument is not allowed here">*</error>b
+a[0], a.x = *b
+
 a.b=*c
 a.b= *c
 a.b =*c

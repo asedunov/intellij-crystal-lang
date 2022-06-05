bfa57f4fc3b93bd6c5a6d18886e0c4df8ffa6d81
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-06-05 18:57:44 +0200

    Analysis: Report enum constants introduced by type reopening
    (cherry picked from commit 06e92c71b34979eb9835cadcfd8e7a87189aeadf)

---
+++ src/testData/analysis/addConstantsToEnum.cr
@@ -0,0 +1,9 @@
+enum <error descr="Enum must have at least one constant">E</error>
+
+end
+
+enum <error descr="Can't add constants to enum definition">E</error>
+  X
+  Y
+  Z
+end
--- src/testData/analysis/nonDynamics/enum.cr
+++ src/testData/analysis/nonDynamics/enum.cr
@@ -1,43 +1,43 @@
-enum X
+enum X1
   E
 end
 
-(enum X
+(enum X2
    E
  end)
 
 begin
-  enum X
+  enum X3
     E
   end
 end
 
-A = enum X
+A = enum X4
   E
 end
 
-a = enum <error descr="Can't declare enum dynamically">X</error>
+a = enum <error descr="Can't declare enum dynamically">X5</error>
   E
 end
 
 if true
-  enum <error descr="Can't declare enum dynamically">X</error>
+  enum <error descr="Can't declare enum dynamically">X6</error>
     E
   end
 else
-  enum <error descr="Can't declare enum dynamically">X</error>
+  enum <error descr="Can't declare enum dynamically">X7</error>
     E
   end
 end
 
 while true
-  enum <error descr="Can't declare enum dynamically">E</error>
+  enum <error descr="Can't declare enum dynamically">X8</error>
     E
   end
 end
 
 class A
-  enum X
+  enum X9
     E
   end
 end

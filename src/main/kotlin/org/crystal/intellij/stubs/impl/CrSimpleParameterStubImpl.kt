cccfcdd6d7e57ff111ff6a01298fc3aac1898c79
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-08-25 22:48:41 +0200

    Stubs: Store parameter kind in stubs
    (cherry picked from commit 7908b49c7e8d6c7f35bd6d0482d00f648fe4c95f)

--- src/main/kotlin/org/crystal/intellij/stubs/impl/CrSimpleParameterStubImpl.kt
+++ src/main/kotlin/org/crystal/intellij/stubs/impl/CrSimpleParameterStubImpl.kt
@@ -1,13 +1,15 @@
 package org.crystal.intellij.stubs.impl
 
 import com.intellij.psi.stubs.StubElement
+import org.crystal.intellij.psi.CrParameterKind
 import org.crystal.intellij.psi.CrSimpleParameter
 import org.crystal.intellij.stubs.api.CrSimpleParameterStub
 import org.crystal.intellij.stubs.elementTypes.CrSimpleParameterElementType
 
 class CrSimpleParameterStubImpl(
     parent: StubElement<*>?,
-    override val hasInitializer: Boolean
+    override val hasInitializer: Boolean,
+    override val kind: CrParameterKind
 ) : CrStubElementImpl<CrSimpleParameter>(parent, CrSimpleParameterElementType), CrSimpleParameterStub {
-    override fun toString() = "CrSimpleParameterStubImpl(hasDefaultValue=$hasInitializer)"
+    override fun toString() = "CrSimpleParameterStubImpl(hasInitializer=$hasInitializer, kind=$kind)"
 }

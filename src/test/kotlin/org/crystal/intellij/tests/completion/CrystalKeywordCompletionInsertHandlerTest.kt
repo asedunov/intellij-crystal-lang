4151547adc5e55cf3df27f2a0857d78a2a663674
Alexey.Sedunov <alexey.sedunov@gmail.com>
2022-11-07 19:15:05 +0100

    Completion: Add insertion handler test for keywords

---
+++ src/test/kotlin/org/crystal/intellij/tests/completion/CrystalKeywordCompletionInsertHandlerTest.kt
@@ -0,0 +1,58 @@
+package org.crystal.intellij.tests.completion
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.junit.Test
+
+class CrystalKeywordCompletionInsertHandlerTest : BasePlatformTestCase() {
+    private fun doTest(beforeText: String, afterText: String) {
+        myFixture.configureByText("a.cr", beforeText)
+        myFixture.completeBasic()
+        if (myFixture.lookup != null) myFixture.finishLookup('\n')
+        myFixture.checkResult(afterText)
+    }
+
+    private infix fun String.expects(after: String) {
+        doTest(this, after)
+    }
+
+    @Test
+    fun testSpaceRequiringKeywords() {
+        "abstract<caret>" expects "abstract <caret>"
+        "alias<caret>" expects "alias <caret>"
+        "annotation<caret>" expects "annotation <caret>"
+        "as<caret>" expects "as <caret>"
+        "def<caret>" expects "def <caret>"
+        "enum<caret>" expects "enum <caret>"
+        "is_a<caret>" expects "is_a? <caret>"
+        "macro<caret>" expects "macro <caret>"
+        "module<caret>" expects "module <caret>"
+        "next<caret>" expects "next <caret>"
+        "[1, 2, 3] of<caret>" expects "[1, 2, 3] of <caret>"
+        "private<caret>" expects "private <caret>"
+        "protected<caret>" expects "protected <caret>"
+        "responds_to<caret>" expects "responds_to? <caret>"
+        "return<caret>" expects "return <caret>"
+        "struct<caret>" expects "struct <caret>"
+        "a = uninitialized<caret>" expects "a = uninitialized <caret>"
+        "unless<caret>" expects "unless <caret>"
+        "until<caret>" expects "until <caret>"
+        "while<caret>" expects "while <caret>"
+        "with<caret>" expects "with <caret>"
+        "yield<caret>" expects "yield <caret>"
+    }
+
+    @Test
+    fun testParenRequiringKeywords() {
+        "asm<caret>" expects "asm(<caret>)"
+        "instance_sizeof<caret>" expects "instance_sizeof(<caret>)"
+        "offsetof<caret>" expects "offsetof(<caret>)"
+        "pointerof<caret>" expects "pointerof(<caret>)"
+        "sizeof<caret>" expects "sizeof(<caret>)"
+        "typeof<caret>" expects "typeof(<caret>)"
+    }
+
+    @Test
+    fun testRequire() {
+        "require<caret>" expects "require \"<caret>\""
+    }
+}

ffc706205d1ee54a0b5f5c9c8b316535ec5be5ae
Alexey.Sedunov <alexey.sedunov@gmail.com>
2022-12-01 15:25:46 +0100

    Misc: Refactor SDK model for better support of multiple tools
    (cherry picked from commit 4365c3144a4db485fe4687773b38fcf57792a197)

--- src/main/kotlin/org/crystal/intellij/util/fileUtils.kt
+++ src/main/kotlin/org/crystal/intellij/util/fileUtils.kt
@@ -1,10 +1,20 @@
 package org.crystal.intellij.util
 
+import com.intellij.util.io.exists
+import com.intellij.util.io.isFile
 import java.nio.file.InvalidPathException
+import java.nio.file.Path
 import java.nio.file.Paths
+
+private val emptyPath: Path = Path.of("")
 
 fun String.toPathOrNull() = try {
     Paths.get(this)
 } catch (e: InvalidPathException) {
     null
 }
+
+fun String.toPathOrEmpty() = toPathOrNull() ?: emptyPath
+
+val Path.isValidFile: Boolean
+    get() = exists() && isFile()

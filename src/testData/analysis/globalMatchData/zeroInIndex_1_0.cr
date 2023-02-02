775942771812cb945eda5f46f5ed66f58c4301af
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-02-02 19:56:46 +0100

    Misc: Fix test data
    (cherry picked from commit 5790f1c015ddac41d1c9a61933dce156024f5eee)

--- src/testData/analysis/globalMatchData/zeroInIndex_1_0.cr
+++ src/testData/analysis/globalMatchData/zeroInIndex_1_0.cr
@@ -9,6 +9,6 @@ $01?
 $012
 $012?
 $10
-<error descr="Global match index with zero at second position is invalid in pre-1.7">$10?</error>
-<error descr="Global match index with zero at second position is invalid in pre-1.7">$102</error>
-<error descr="Global match index with zero at second position is invalid in pre-1.7">$102?</error>
+<error descr="Global match index with zero at second position is invalid before 1.7">$10?</error>
+<error descr="Global match index with zero at second position is invalid before 1.7">$102</error>
+<error descr="Global match index with zero at second position is invalid before 1.7">$102?</error>

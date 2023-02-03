fc9b14270093b086e7ad571d50b2fef07192a032
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-02-03 12:55:08 +0100

    Quick Fixes: Add action to convert named tuple to hash literal (#35)

--- src/testData/analysis/collectionLiterals/customLiterals.cr
+++ src/testData/analysis/collectionLiterals/customLiterals.cr
@@ -1,9 +1,9 @@
-<error descr="Can't use named tuple syntax for Hash-like literal, use '=>' instead">Foo { a: 123 }</error>
+<error descr="Tuple syntax is not supported for Hash-like literal">Foo { a: 123 }</error>
 
 Foo { a => 123 }
 
 Foo { 1, 2, 3 }
 
-<error descr="Can't use named tuple syntax for Hash-like literal, use '=>' instead">Foo { "123": 123 }</error>
+<error descr="Tuple syntax is not supported for Hash-like literal">Foo { "123": 123 }</error>
 
 Foo { 123 => 123 }

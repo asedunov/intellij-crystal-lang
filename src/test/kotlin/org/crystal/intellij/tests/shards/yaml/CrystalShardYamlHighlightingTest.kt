938c3ef8361113a41bad8871a653a40ad33b2e0f
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-02-16 19:41:22 +0100

    Inspections: Report missing keys in shard.yml (#39)
    (cherry picked from commit 53750263d1b383924faf0fd0b90dd2acc875b3a4)

---
+++ src/test/kotlin/org/crystal/intellij/tests/shards/yaml/CrystalShardYamlHighlightingTest.kt
@@ -0,0 +1,30 @@
+package org.crystal.intellij.tests.shards.yaml
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.crystal.intellij.shards.yaml.inspections.ShardYamlMissingKeysInspection
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import java.io.File
+
+@RunWith(Parameterized::class)
+class CrystalShardYamlHighlightingTest(private val testFile: File) : BasePlatformTestCase() {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun testFiles() = getShardYamlTestFilesAsParameters("shards/yaml/highlighting")
+    }
+
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(
+            ShardYamlMissingKeysInspection::class.java
+        )
+    }
+
+    @Test
+    fun testHighlighting() {
+        myFixture.configureShardYml(testFile)
+        myFixture.checkHighlighting(true, false, true)
+    }
+}

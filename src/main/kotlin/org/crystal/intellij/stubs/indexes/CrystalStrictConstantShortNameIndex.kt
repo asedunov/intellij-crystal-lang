66b174bd85a7c1f2374b6fc38fb86817b4169a38
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-03-01 02:35:49 +0100

    Misc: Turn Kotlin objects registered as extensions into classes
    (cherry picked from commit d1cdf1200e0683ab1a3ec8c3a7b738d81f89e2f9)

--- src/main/kotlin/org/crystal/intellij/stubs/indexes/CrystalStrictConstantShortNameIndex.kt
+++ src/main/kotlin/org/crystal/intellij/stubs/indexes/CrystalStrictConstantShortNameIndex.kt
@@ -1,20 +1,13 @@
 package org.crystal.intellij.stubs.indexes
 
-import com.intellij.openapi.project.Project
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.stubs.StringStubIndexExtension
-import com.intellij.psi.stubs.StubIndex
-import com.intellij.psi.stubs.StubIndexKey
 import org.crystal.intellij.psi.CrDefinitionWithFqName
 
-object CrystalStrictConstantShortNameIndex : StringStubIndexExtension<CrDefinitionWithFqName>() {
-    private val KEY = StubIndexKey.createIndexKey<String, CrDefinitionWithFqName>(
-        CrystalStrictConstantShortNameIndex::class.java.canonicalName
+class CrystalStrictConstantShortNameIndex : CrystalStringStubIndexExtensionBase<CrDefinitionWithFqName>() {
+    companion object Helper: HelperBase<CrDefinitionWithFqName>(
+        CrystalStrictConstantShortNameIndex::class.java,
+        CrDefinitionWithFqName::class.java
     )
 
-    override fun getKey() = KEY
-
-    override fun get(s: String, project: Project, scope: GlobalSearchScope): Collection<CrDefinitionWithFqName> {
-        return StubIndex.getElements(KEY, s, project, scope, CrDefinitionWithFqName::class.java)
-    }
+    override val helper
+        get() = Helper
 }

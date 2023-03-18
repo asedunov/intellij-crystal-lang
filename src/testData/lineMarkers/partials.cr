1bceab5d844da14c022c70b9f1f3c869099dfde7
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-03-18 13:45:07 +0100

    Editor: Implement line markers for partial type definitions (#43)

---
+++ src/testData/lineMarkers/partials.cr
@@ -0,0 +1,44 @@
+class CSingle
+end
+
+class <lineMarker descr="Has partial declarations">CMulti</lineMarker>
+end
+
+class <lineMarker descr="Has partial declarations">CMulti</lineMarker>
+end
+
+struct SSingle
+end
+
+struct <lineMarker descr="Has partial declarations">SMulti</lineMarker>
+end
+
+struct <lineMarker descr="Has partial declarations">SMulti</lineMarker>
+end
+
+module MSingle
+end
+
+module <lineMarker descr="Has partial declarations">MMulti</lineMarker>
+end
+
+module <lineMarker descr="Has partial declarations">MMulti</lineMarker>
+end
+
+lib LSingle
+end
+
+lib <lineMarker descr="Has partial declarations">LMulti</lineMarker>
+end
+
+lib <lineMarker descr="Has partial declarations">LMulti</lineMarker>
+end
+
+annotation ASingle
+end
+
+annotation <lineMarker descr="Has partial declarations">AMulti</lineMarker>
+end
+
+annotation <lineMarker descr="Has partial declarations">AMulti</lineMarker>
+end

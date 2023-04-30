c7cebff81bc21cf116aa59919438e985f85aa35c
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-04-30 19:37:40 +0200

    Run: Add working directory to run configuration options (#53)
    (cherry picked from commit 22acdf3398c09a68618486134642961bebde6583)

--- src/main/kotlin/org/crystal/intellij/run/CrystalFileRunConfigurationEditor.kt
+++ src/main/kotlin/org/crystal/intellij/run/CrystalFileRunConfigurationEditor.kt
@@ -14,6 +14,7 @@ class CrystalFileRunConfigurationEditor : CrystalFileRunConfigurationEditorBase<
         addFileToRun()
         addProgramArguments()
         addEnvironmentVariables()
+        addWorkingDirectory()
     }
 
     private fun LayoutBuilder.addProgramArguments() {

f01f9b0cf4b9efab033a417e122832554eece2c4
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-04-30 15:02:30 +0200

    Build: Implement build run configuration (#52)
    (cherry picked from commit 2003a243ecd0a27ae2a3be8e44a83dea96f15e60)

---
+++ src/main/kotlin/org/crystal/intellij/run/CrystalFileBuildConfiguration.kt
@@ -0,0 +1,43 @@
+package org.crystal.intellij.run
+
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.openapi.project.Project
+import org.crystal.intellij.util.addNestedValue
+import org.crystal.intellij.util.getNestedString
+import org.jdom.Element
+
+class CrystalFileBuildConfiguration(
+    project: Project,
+    name: String,
+    factory: ConfigurationFactory
+) : CrystalFileRunConfigurationBase(project, name, factory) {
+    var outputFileName: String = ""
+
+    override val showProgress: Boolean
+        get() = true
+
+    override val suggestedNamePrefix: String
+        get() = "Build"
+
+    override val commandArgument: String
+        get() = "build"
+
+    override fun readExternal(element: Element) {
+        super.readExternal(element)
+        outputFileName = element.getNestedString("outputFileName") ?: ""
+    }
+
+    override fun writeExternal(element: Element) {
+        super.writeExternal(element)
+        element.addNestedValue("outputFileName", outputFileName)
+    }
+
+    override fun patchArgumentList(arguments: ArrayList<Any>) {
+        if (outputFileName.isNotBlank()) {
+            arguments += "-o"
+            arguments += outputFileName
+        }
+    }
+
+    override fun getConfigurationEditor() = CrystalFileBuildConfigurationEditor()
+}

6dfa6cb6e264f88d4bdb558ee45195b0cc93ba3e
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-06-08 17:02:11 +0200

    Analysis: When macro reference is not resolve exactly, resolve to candidates (#59)
    (cherry picked from commit d0d2dcae9f9dffe1fed655829e4718f06380836e)

--- src/main/kotlin/org/crystal/intellij/psi/CrReferenceElement.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrReferenceElement.kt
@@ -4,4 +4,6 @@ import org.crystal.intellij.resolve.symbols.CrSym
 
 sealed interface CrReferenceElement : CrElement {
     fun resolveSymbol(): CrSym<*>?
+
+    fun resolveCandidates(): List<CrSym<*>> = listOfNotNull(resolveSymbol())
 }

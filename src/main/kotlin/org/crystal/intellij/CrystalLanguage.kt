c6abaebf3a1eff6cc60bf357c316931d11b297f7
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-07-14 17:41:54 +0200

    Misc: Add readResolve() to CrystalLanguage object

--- src/main/kotlin/org/crystal/intellij/CrystalLanguage.kt
+++ src/main/kotlin/org/crystal/intellij/CrystalLanguage.kt
@@ -3,5 +3,7 @@ package org.crystal.intellij
 import com.intellij.lang.Language
 
 object CrystalLanguage : Language("Crystal") {
+    private fun readResolve(): Any = CrystalLanguage
+
     override fun isCaseSensitive() = true
 }

b7e4f4900181dac22ac485dab79da3ffcb769c72
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-07-14 13:53:13 +0200

    General: Update Intellij Gradle plugin to 1.15.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij") version "1.15.0"
     java
     kotlin("jvm") version "1.8.21"
 }

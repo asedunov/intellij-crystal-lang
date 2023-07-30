0a59820a1b03cc4c2b1d1591d5faaf2cda2a56f0
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-07-30 19:34:34 +0200

    General: Update Kotlin version to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.intellij") version "1.15.0"
     java
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
 }
 
 group = "org.crystal.intellij"

61306d26b3118ec5cbd22e5d267060ea0c40818e
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-08-28 15:16:22 +0200

    Structure View: Support expressions in type argument lists (#64)
    (cherry picked from commit b1cac3adbd1886740c2806918306eaae3e76aa80)

--- src/main/kotlin/org/crystal/intellij/psi/CrSizeExpression.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrSizeExpression.kt
@@ -4,4 +4,7 @@ import com.intellij.lang.ASTNode
 
 class CrSizeExpression(node: ASTNode) : CrExpressionImpl(node), CrTypeArgument {
     override fun accept(visitor: CrVisitor) = visitor.visitSizeExpression(this)
+
+    val typeElement: CrTypeElement<*>?
+        get() = childOfType()
 }

e4eb5428a20bef9b1199c69873f162c17a58e04f
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-08-29 21:07:27 +0200

    Completion: Fix keyword suggestions for multi-argument `typeof` (part 2)
    (cherry picked from commit 581a3a01fffc391d3feef7436e0dfe3efb213cc2)

--- src/main/kotlin/org/crystal/intellij/psi/CrTypeofExpression.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrTypeofExpression.kt
@@ -1,10 +1,11 @@
 package org.crystal.intellij.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.util.containers.JBIterable
 
 class CrTypeofExpression(node: ASTNode) : CrExpressionImpl(node) {
     override fun accept(visitor: CrVisitor) = visitor.visitTypeofExpression(this)
 
-    val expression: CrExpression?
-        get() = childOfType()
+    val expressions: JBIterable<CrExpression>
+        get() = childrenOfType()
 }

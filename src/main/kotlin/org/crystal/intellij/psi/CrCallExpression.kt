153486cdf316c08c942c8269348195d71598a722
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-08-30 02:41:33 +0200

    Parser: Introduce CrExpressionWithReceiver
    (cherry picked from commit 0c6018a87a1bd687e8ab7344d2b57920af2be9c6)

--- src/main/kotlin/org/crystal/intellij/psi/CrCallExpression.kt
+++ src/main/kotlin/org/crystal/intellij/psi/CrCallExpression.kt
@@ -5,9 +5,6 @@ import com.intellij.lang.ASTNode
 class CrCallExpression(node: ASTNode) : CrCallLikeExpression(node), CrVisibilityHolder {
     override fun accept(visitor: CrVisitor) = visitor.visitCallExpression(this)
 
-    override val ownReceiver: CrExpression?
-        get() = childOfType<CrExpression>().takeUnless { it == lastChild }
-
     override val argumentList: CrArgumentList?
         get() = childOfType()
 

bba75cd783108dc8ed42602af9854b6af97c696d
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-09-15 00:13:21 +0200

    Parser: Allow string interpolation at the beginning of heredoc (#72)
    (cherry picked from commit 1ed4b82fd316f7eee061c0367fffdf10b29bda08)

--- src/main/gen/org/crystal/intellij/lang/lexer/_CrystalLexer10.java
+++ src/main/gen/org/crystal/intellij/lang/lexer/_CrystalLexer10.java
@@ -1469,14 +1469,14 @@ public class _CrystalLexer10 extends CrystalLexerBase {
       heredocIds.pop();
       yypushbackAndBegin(HEREDOC_END_ID);
       zzStartRead = zzMarkedPos - blockLength;
-      return handle(CR_HEREDOC_BODY);
+      return handle(CR_HEREDOC_RAW);
     }
     else {
       blockLength += yylength();
       if (isEof) {
         heredocIds.clear();
         zzStartRead = zzMarkedPos - blockLength;
-        return handle(CR_HEREDOC_BODY);
+        return handle(CR_HEREDOC_RAW);
       }
       return null;
     }
@@ -2616,7 +2616,7 @@ public class _CrystalLexer10 extends CrystalLexerBase {
           case 131:
             { if (isFullHeredoc()) {
       return blockLength != 0
-        ? closePrecedingBlockToken(CR_HEREDOC_BODY)
+        ? closePrecedingBlockToken(CR_HEREDOC_RAW)
         : enterBlock(INTERPOLATION_BLOCK, CR_INTERPOLATION_START, CR_INTERPOLATION_END);
     }
     IElementType type = consumeHeredocPortion(zzInput == YYEOF);

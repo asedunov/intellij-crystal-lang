a76026ff9ecead881cb9fc5d03087d0066ce95a4
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-09-14 12:51:09 +0200

    Misc: Get rid of CrLiteralExpression
    (cherry picked from commit f52302b42acb894f35bda477aa8ce803ef5b0829)

--- src/main/kotlin/org/crystal/intellij/lang/psi/CrHeredocExpression.kt
+++ src/main/kotlin/org/crystal/intellij/lang/psi/CrHeredocExpression.kt
@@ -2,6 +2,6 @@ package org.crystal.intellij.lang.psi
 
 import com.intellij.lang.ASTNode
 
-class CrHeredocExpression(node: ASTNode) : CrExpressionImpl(node), CrLiteralExpression {
+class CrHeredocExpression(node: ASTNode) : CrExpressionImpl(node) {
     override fun accept(visitor: CrVisitor) = visitor.visitHeredocExpression(this)
 }
--- src/main/kotlin/org/crystal/intellij/lang/psi/CrTupleExpression.kt
+++ src/main/kotlin/org/crystal/intellij/lang/psi/CrTupleExpression.kt
@@ -3,7 +3,7 @@ package org.crystal.intellij.lang.psi
 import com.intellij.lang.ASTNode
 import com.intellij.util.containers.JBIterable
 
-class CrTupleExpression(node: ASTNode) : CrExpressionImpl(node), CrLiteralExpression {
+class CrTupleExpression(node: ASTNode) : CrExpressionImpl(node) {
     override fun accept(visitor: CrVisitor) = visitor.visitTupleExpression(this)
 
     val expressions: JBIterable<CrExpression>

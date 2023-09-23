e8d298b7587bd16a22915b5583b3161cf6c323ae
Alexey.Sedunov <alexey.sedunov@gmail.com>
2023-09-23 20:16:53 +0200

    Parser: Place synthetic arguments inside CrMultiParameter
    (cherry picked from commit a8ca4cad07a77cac2559ce91dd6ce30e68f990fd)

--- src/testData/parser/CallsAndRefs_1_9.cr
+++ src/testData/parser/CallsAndRefs_1_9.cr
@@ -2,3 +2,21 @@ foo { |(a, (b, (c, d)))| }
 
 
 foo { |(a, *b, c)| }
+
+
+foo { |x, x| }
+
+
+foo { |x, (x)| }
+
+
+foo { |(x, x)| }
+
+
+foo { |a, (b, c), (d, e)| a; b; c; d; e }
+
+
+foo { |(_, c)| c }
+
+
+foo { |(_, c, )| c }
